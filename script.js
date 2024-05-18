57ce0f468f652431b196fafa1ec5729f4440b466
askhat <askhat.9812@gmail.com>
2024-05-18 21:06:41 +0500

    Initial

---
+++ script.js
@@ -0,0 +1,102 @@
+const apiUrl = 'https://7103.api.greenapi.com';
+const result = $('#result');
+
+$('#getSettings').on('click', () => {
+  // GET {{apiUrl}}/waInstance{{idInstance}}/getSettings/{{apiTokenInstance}}
+  const idInstance = $('#idInstance').val();
+  const apiToken = $('#apiToken').val();
+  if (idInstance == '' || apiToken == '') {
+    alert('Установите ID Инстанса и Ваш Токен')
+  } else {
+    $.ajax({
+      url: `${apiUrl}/waInstance${idInstance}/getSettings/${apiToken}`,
+      type: 'GET',
+      success: function(data) {
+        result.val(JSON.stringify(data, null, 4));
+      },
+      error: function(jqXHR, exception) {
+        result.val('Error :(');
+      }
+    })
+  }
+});
+
+$('#getStateInstance').on('click', () => {
+  // GET {{apiUrl}}/waInstance{{idInstance}}/getStateInstance/{{apiTokenInstance}}
+  const idInstance = $('#idInstance').val();
+  const apiToken = $('#apiToken').val();
+  if (idInstance == '' || apiToken == '') {
+    alert('Установите ID Инстанса и Ваш Токен')
+  } else {
+    $.ajax({
+      url: `${apiUrl}/waInstance${idInstance}/getStateInstance/${apiToken}`,
+      type: 'GET',
+      success: function(data) {
+        result.val(JSON.stringify(data, null, 4));
+      },
+      error: function(jqXHR, exception) {
+        result.val('Error :(');
+      }
+    })
+  }
+})
+
+$('#sendMessage').on('click', () => {
+  // POST {{apiUrl}}/waInstance{{idInstance}}/sendMessage/{{apiTokenInstance}}
+  const idInstance = $('#idInstance').val();
+  const apiToken = $('#apiToken').val();
+  const phoneNumber = $('#sendMessagePhone').val();
+  const message = $('#message').val();
+
+  const data = {
+    chatId: phoneNumber + '@c.us',
+    message: message
+  }
+
+  if (idInstance == '' || apiToken == '') {
+    alert('Установите ID Инстанса и Ваш Токен')
+  } else if (phoneNumber == '' || message == '') {
+    alert('Заполните нужные поля!');
+  } else {
+    $.ajax({
+      url: `${apiUrl}/waInstance${idInstance}/sendMessage/${apiToken}`,
+      type: 'POST',
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+      success: function(data) {
+        result.val(JSON.stringify(data, null, 4));
+      }
+    })
+  }
+})
+
+
+$('#sendFileByUrl').on('click', () => {
+  // POST {{apiUrl}}/waInstance{{idInstance}}/sendFileByUrl/{{apiTokenInstance}}
+  const idInstance = $('#idInstance').val();
+  const apiToken = $('#apiToken').val();
+  const phoneNumber = $('#sendFilePhone').val();
+  const fileURL = $('#fileURL').val();
+
+  const data = {
+    chatId: phoneNumber + '@c.us',
+    fileName: (Math.random() + 1).toString(36).substring(2),
+    urlFile: fileURL
+  }
+
+  if (idInstance == '' || apiToken == '') {
+    alert('Установите ID Инстанса и Ваш Токен')
+  } else if (phoneNumber == '' || fileURL == '') {
+    alert('Заполните нужные поля!');
+  } else {
+    $.ajax({
+      url: `${apiUrl}/waInstance${idInstance}/sendFileByUrl/${apiToken}`,
+      type: 'POST',
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+      success: function(data) {
+        result.val(JSON.stringify(data, null, 4));
+      }
+    })
+  }
+})
